fa1513a25076079febebff931de0f01a210ba73e
rohitpritamrai <rohitpritamrai@gmail.com>
2021-07-29 20:21:08 +0530

    Java Program to Print two space-separated integers on one line: the minimum sum and the maximum sum of  of  elements.

---
+++ src/MinMaxSum.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+public class MinMaxSum {
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<Integer> arr) {
+        // Write your code here
+        long min = Collections.min(arr);
+        long max = Collections.max(arr);
+        long sum = 0;
+
+        for (Integer integer : arr) {
+            sum += integer;
+        }
+        System.out.println((sum-max)+" "+(sum-min));
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+
+        miniMaxSum(arr);
+
+        bufferedReader.close();
+    }
+
+}
